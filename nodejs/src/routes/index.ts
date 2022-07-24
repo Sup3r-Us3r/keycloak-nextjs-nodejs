1153af7efcf368f45f954d315a5369099649ca16
Sup3r-Us3r <maydersonmello@gmail.com>
2022-07-23 21:04:45 -0300

    feat: protect api routes with roles

--- nodejs/src/routes/index.ts
+++ nodejs/src/routes/index.ts
@@ -1,21 +1,29 @@
 import { Router } from 'express';
+import { getKeycloak } from '../config/keycloak';
 
 const router = Router();
+const keycloak = getKeycloak();
 
 router.get('/anonymous', (request, response) => {
   return response.send('Hello Anonymous');
 });
 
-router.get('/user', (request, response) => {
+router.get('/user', keycloak.protect('user'), (request, response) => {
   return response.send('Hello User');
 });
 
-router.get('/admin', (request, response) => {
+router.get('/admin', keycloak.protect('admin'), (request, response) => {
   return response.send('Hello Admin');
 });
 
-router.get('/all-user', (request, response) => {
-  return response.send('Hello All User');
-});
+router.get(
+  '/all-user',
+  keycloak.protect(['user', 'admin'] as any),
+  (request, response) => {
+    console.log(request.kauth.grant.access_token.token);
+
+    return response.send('Hello All User');
+  }
+);
 
 export { router as routes };
