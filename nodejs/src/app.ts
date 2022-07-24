9811901bfe091c3492ea178944d7d43ce246031e
Sup3r-Us3r <maydersonmello@gmail.com>
2022-07-23 21:15:46 -0300

    refactor: enable keycloak middleware

--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -1,13 +1,28 @@
 import express from 'express';
+import session from 'express-session';
 import cors from 'cors';
 
+import { getKeycloak, memoryStore } from './config/keycloak';
 import { routes } from './routes';
 
 const app = express();
+const keycloak = getKeycloak();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+  // Generate secret: openssl rand -base64 32
+  secret: 'IECSsT+IBPxmfuULAqrDebeXFCWSFBDDTb6FpEQ+Kd4=',
+  resave: false,
+  saveUninitialized: true,
+  store: memoryStore,
+  // cookie: {
+  //   maxAge: 1000 * 60 * 10 // 10min
+  // }
+}));
+app.use(keycloak.middleware());
 
 app.use(routes);
 
