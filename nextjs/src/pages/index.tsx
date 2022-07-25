139ad385eafbbb424a60508e2edfb7ad1c74add6
Sup3r-Us3r <maydersonmello@gmail.com>
2022-07-25 20:36:31 -0300

    feat: force login when refresh token gives error

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
+import { useSession, signIn } from 'next-auth/react';
+
 import { Layout } from '../components/Layout';
 
 const IndexPage = () => {
+  const session = useSession();
+
+  useEffect(() => {
+    if (session?.data?.error === 'RefreshAccessTokenError') {
+      // Force sign in to hopefully resolve error
+      signIn('keycloak');
+    }
+  }, [session]);
+
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1 className="mt-5">Hello Next.js + Keycloak 👋</h1>
