bee925e02cb6d2c11f253735b895d34583511a2b
Sup3r-Us3r <maydersonmello@gmail.com>
2022-07-28 12:40:54 -0300

    fix: keycloak protect with more than one role

--- nodejs/src/routes/index.ts
+++ nodejs/src/routes/index.ts
@@ -1,24 +1,30 @@
-import { Router } from 'express';
+import { Router, Request } from 'express';
+import { Token } from 'keycloak-connect';
+
 import { getKeycloak } from '../config/keycloak';
 
 const router = Router();
 const keycloak = getKeycloak();
 
-router.get('/anonymous', (request, response) => {
+router.get('/anonymous', (_request, response) => {
   return response.send('Hello Anonymous');
 });
 
-router.get('/user', keycloak.protect('user'), (request, response) => {
+router.get('/user', keycloak.protect('user'), (_request, response) => {
   return response.send('Hello User');
 });
 
-router.get('/admin', keycloak.protect('admin'), (request, response) => {
+router.get('/admin', keycloak.protect('admin'), (_request, response) => {
   return response.send('Hello Admin');
 });
 
 router.get(
   '/all-user',
-  keycloak.protect(['user', 'admin'] as any),
+  keycloak.protect(
+    (accessToken: Token, _request: Request) => {
+      return accessToken.hasRole('user') && accessToken.hasRole('admin');
+    }
+  ),
   (request, response) => {
     console.log(request.kauth.grant.access_token.token);
 
