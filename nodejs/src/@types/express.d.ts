406a7e6ddbf03bbcd501615f5f9d6272d3e1ed49
Sup3r-Us3r <maydersonmello@gmail.com>
2022-07-28 17:31:29 -0300

    refactor: add new types in kauth

--- nodejs/src/@types/express.d.ts
+++ nodejs/src/@types/express.d.ts
@@ -1,12 +1,44 @@
 declare namespace Express {
   export interface Request {
+    keycloakRolesDefined: string[];
     kauth: {
       grant: {
         access_token: {
           token: string;
           clientId: string;
-          header: object;
-          content: object;
+          header: {
+            alg: string;
+            typ: string;
+            kid: string;
+          };
+          content: {
+            exp: number;
+            iat: number;
+            jti: string;
+            iss: string;
+            aud: string;
+            sub: string;
+            typ: string;
+            azp: string;
+            session_state: string;
+            acr: string;
+            'allowed-origins': string[];
+            realm_access: {
+              roles: string[];
+            };
+            resource_access: {
+              nodejs: {
+                roles: string[];
+              };
+              account: {
+                roles: string[];
+              }
+            };
+            scope: string;
+            sid: string;
+            email_verified: boolean;
+            preferred_username: string;
+          };
           signature: Buffer | any;
           signed: string;
         };
